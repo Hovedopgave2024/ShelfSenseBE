42e3b94c9ced6c733805fd3a5f64b264cc3751a3
UlrikLn <ulrik9000@hotmail.com>
2024-11-13 11:59:47 +0100

    Added RequestBody

--- src/main/java/com/example/shelfsensebe/Controller/UserController.java
+++ src/main/java/com/example/shelfsensebe/Controller/UserController.java
@@ -15,19 +15,22 @@ public class UserController
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/createUser")
-    public User createUser(@RequestBody User user)
+    @PostMapping("/users")
+    public User users(@RequestBody User user)
     {
+        if (userRepository.findByName(user.getName()).isPresent()) {
+            throw new IllegalArgumentException("User already exists");
+        }
         return userRepository.save(user);
     }
 
 
-    @PostMapping("/loginUser")
-    public ResponseEntity<String> login(@RequestParam String name, @RequestParam String password) {
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
 
-        Optional<User> userOptional = userRepository.findByName(name);
+        Optional<User> userOptional = userRepository.findByName(user.getName());
 
-        if (userOptional.isPresent() && userOptional.get().getPassword().equals(password)) {
+        if (userOptional.isPresent() && userOptional.get().getPassword().equals(user.getPassword())) {
             return ResponseEntity.ok("Login successful");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
