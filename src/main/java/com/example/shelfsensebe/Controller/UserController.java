7d29f30faf998d442d4d4d83bb758e9f54330afa
UlrikLn <ulrik9000@hotmail.com>
2024-11-13 10:44:06 +0100

    made usercontroller and other related classes

---
+++ src/main/java/com/example/shelfsensebe/Controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.shelfsensebe.Controller;
+
+import com.example.shelfsensebe.Model.User;
+import com.example.shelfsensebe.Repository.UserRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class UserController
+{
+    @Autowired
+    UserRespository userRespository;
+
+    @PostMapping("/createUser")
+    public User createUser(@RequestBody User user)
+    {
+        return userRespository.save(user);
+    }
+
+    @PostMapping("/loginUser")
+    public ResponseEntity<String> login(@RequestParam String name, @RequestParam String password) {
+
+        Optional<User> userOptional = userRespository.findByUsername(name);
+
+        if (userOptional.isPresent() && userOptional.get().getPassword().equals(password)) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
+}
---
+++ src/main/java/com/example/shelfsensebe/Repository/UserRespository.java
@@ -0,0 +1,14 @@
+package com.example.shelfsensebe.Repository;
+
+import com.example.shelfsensebe.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public interface UserRespository extends JpaRepository<User, Integer>
+{
+
+    Optional<User> findByUsername(String name);
+}
