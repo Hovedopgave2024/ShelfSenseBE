19f5f627fc2087fe5c2784a15ce6a1cab19ca8c7
PedroLorenzen <christianplorenzen@outlook.com>
2024-11-14 09:49:55 +0100

    added servlet for session and added session code in user controller

--- src/main/java/com/example/shelfsensebe/Controller/UserController.java
+++ src/main/java/com/example/shelfsensebe/Controller/UserController.java
@@ -1,9 +1,9 @@
 package com.example.shelfsensebe.Controller;
-
 import com.example.shelfsensebe.Model.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import com.example.shelfsensebe.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,21 +20,40 @@ public class UserController
     public User users(@RequestBody User user)
     {
         if (userRepository.findByName(user.getName()).isPresent()) {
-            throw new IllegalArgumentException("User already exists");
+            throw new IllegalArgumentException("User with name: " + user.getName() + " already exists");
         }
         return userRepository.save(user);
     }
 
     @CrossOrigin
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
+    public ResponseEntity<String> login(@RequestBody User user, HttpSession session) {
 
         Optional<User> userOptional = userRepository.findByName(user.getName());
 
         if (userOptional.isPresent() && userOptional.get().getPassword().equals(user.getPassword())) {
+            session.setAttribute("user", userOptional.get().getName());
             return ResponseEntity.ok("Login successful");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
+
+    @CrossOrigin
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        session.invalidate();
+        return ResponseEntity.ok("Logout successful");
+    }
+
+    @CrossOrigin
+    @GetMapping("/session-status")
+    public ResponseEntity<String> sessionStatus(HttpSession session) {
+        String userName = (String) session.getAttribute("user");
+        if (userName != null) {
+            return ResponseEntity.ok("User " + userName + " is logged in");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("No active session");
         }
     }
 }
