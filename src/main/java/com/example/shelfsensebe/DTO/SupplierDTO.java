2327084d169ad2c951c4359215f5bb1fca007da8
PedroLorenzen <christianplorenzen@outlook.com>
2024-11-22 14:04:40 +0100

    update supplier dto

--- src/main/java/com/example/shelfsensebe/DTO/SupplierDTO.java
+++ src/main/java/com/example/shelfsensebe/DTO/SupplierDTO.java
@@ -7,13 +7,15 @@ import java.sql.Date;
 @Data
 public class SupplierDTO {
     private int id;
-    private int safetyStock;
+    private int supplierStock;
+    private int supplierSafetyStock;
     private int supplierIncomingStock;
     private Date supplierIncomingDate;
 
-    public SupplierDTO(int id, int safetyStock, int supplierIncomingStock, Date supplierIncomingDate) {
+    public SupplierDTO(int id, int supplierStock, int supplierSafetyStock, int supplierIncomingStock, Date supplierIncomingDate) {
         this.id = id;
-        this.safetyStock = safetyStock;
+        this.supplierStock = supplierStock;
+        this.supplierSafetyStock = supplierSafetyStock;
         this.supplierIncomingStock = supplierIncomingStock;
         this.supplierIncomingDate = supplierIncomingDate;
     }
