94a600258f6eb4ed496716bd72343d7ba603d66e
PedroLorenzen <christianplorenzen@outlook.com>
2024-11-22 14:28:10 +0100

    change name of config file

--- src/main/java/com/example/shelfsensebe/config/MouserApiConfig.java
+++ src/main/java/com/example/shelfsensebe/config/MouserApiConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebApiConfig {
+public class MouserApiConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder.baseUrl("https://api.mouser.com/api/v2").build();
