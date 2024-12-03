e587565919bbd411b99915ae80feb8cbac6b30a8
PedroLorenzen <christianplorenzen@outlook.com>
2024-12-03 21:28:38 +0100

    add password validator

---
+++ src/main/java/com/example/shelfsensebe/utility/PasswordValidator.java
@@ -0,0 +1,11 @@
+package com.example.shelfsensebe.utility;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordValidator {
+    public boolean isValidPassword(String password) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+        return password.matches(regex);
+    }
+}
