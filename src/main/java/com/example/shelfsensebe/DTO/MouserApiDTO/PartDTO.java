41c2b31b82ba44c99432bba3c0154dc6bcde452b
PedroLorenzen <christianplorenzen@outlook.com>
2024-12-08 13:25:08 +0100

    refactor api call to use DTOs for clarity and best practice

---
+++ src/main/java/com/example/shelfsensebe/DTO/MouserApiDTO/PartDTO.java
@@ -0,0 +1,21 @@
+package com.example.shelfsensebe.DTO.MouserApiDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class PartDTO {
+    private String Manufacturer;
+    private String ManufacturerPartNumber;
+    private int AvailabilityInStock;
+    private List<AvailabilityOnOrderDTO> AvailabilityOnOrder;
+
+    public PartDTO(String Manufacturer, String ManufacturerPartNumber, int AvailabilityInStock, List<AvailabilityOnOrderDTO> AvailabilityOnOrder) {
+        this.Manufacturer = Manufacturer;
+        this.ManufacturerPartNumber = ManufacturerPartNumber;
+        this.AvailabilityInStock = AvailabilityInStock;
+        this.AvailabilityOnOrder = AvailabilityOnOrder;
+    }
+}
---
+++ src/main/java/com/example/shelfsensebe/DTO/MouserApiDTO/SearchResultDTO.java
@@ -0,0 +1,18 @@
+package com.example.shelfsensebe.DTO.MouserApiDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class SearchResultDTO {
+    private int NumberOfResult;
+    private List<PartDTO> Parts;
+
+    public SearchResultDTO(int NumberOfResult, List<PartDTO> Parts) {
+        this.NumberOfResult = NumberOfResult;
+        this.Parts = Parts;
+    }
+}
+
