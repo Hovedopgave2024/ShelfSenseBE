d9c10026bf9885ce0882bfb520187cd34031c3d6
PedroLorenzen <christianplorenzen@outlook.com>
2024-12-20 08:58:36 +0100

    add space for test

--- src/main/java/com/example/shelfsensebe/config/MouserApiConfig.java
+++ src/main/java/com/example/shelfsensebe/config/MouserApiConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class MouserApiConfig {
+    
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder.baseUrl("https://api.mouser.com/api/v2").build();
